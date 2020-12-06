a7287f1ff69bb11be9b605c84b57f65b70ac89c8
Alex Wayne <alex@beautifulpixel.com>
2020-12-05 23:46:45 -0800

    improve day 3 tests

--- day3/index.spec.ts
+++ day3/index.spec.ts
@@ -1,53 +1,31 @@
-import { readInput } from "../common"
-
+import { describeDay } from "../spec-helper"
 import { countTrees, solve1, solve2 } from "."
 
-const sample = `
-..##.......
-#...#...#..
-.#....#..#.
-..#.#...#.#
-.#...##..#.
-..#.##.....
-.#.#.#....#
-.#........#
-#.##...#...
-#...##....#
-.#..#...#.#
-`.trim()
-
-describe("day 3", () => {
-  describe("countTrees", () => {
-    it("counts the trees as you progress down the mountain", () => {
-      const simpleMap = `
+describeDay(
+  3,
+  {
+    solve: solve1,
+    example: 7,
+    puzzle: 176,
+  },
+  {
+    solve: solve2,
+    example: 336,
+    puzzle: 5872458240,
+  },
+  () => {
+    describe("countTrees", () => {
+      it("counts the trees as you progress down the mountain", () => {
+        const simpleMap = `
 .#.
 #.#
 #.#
-      `.trim()
+`.trim()
 
-      expect(countTrees(simpleMap, 1, 1)).toEqual(1)
-      expect(countTrees(simpleMap, 0, 1)).toEqual(2)
-      expect(countTrees(simpleMap, 1, 2)).toEqual(0)
+        expect(countTrees(simpleMap, 1, 1)).toEqual(1)
+        expect(countTrees(simpleMap, 0, 1)).toEqual(2)
+        expect(countTrees(simpleMap, 1, 2)).toEqual(0)
+      })
     })
-  })
-
-  describe("part 1", () => {
-    it("finds valid sample passwords", () => {
-      expect(solve1(sample)).toEqual(7)
-    })
-
-    it("finds the puzzle answer", () => {
-      expect(solve1(readInput(__dirname))).toEqual(176)
-    })
-  })
-
-  describe("part 2", () => {
-    it("finds valid sample passwords", () => {
-      expect(solve2(sample)).toEqual(336)
-    })
-
-    it("finds the puzzle answer", () => {
-      expect(solve2(readInput(__dirname))).toEqual(5872458240)
-    })
-  })
-})
+  },
+)
