71105b4e0eae5af6d4a80df2c23f3fd46a529bef
Alex Wayne <alex@beautifulpixel.com>
2020-12-05 23:35:37 -0800

    fix day 2 tests

--- day2/index.spec.ts
+++ day2/index.spec.ts
@@ -5,59 +5,47 @@ import {
   solve2,
   validateCharacterLocations,
   validateMinMax,
-} from "../day2"
+} from "."
+import { describeDay } from "../spec-helper"
 
-const sample = `
-1-3 a: abcde
-1-3 b: cdefg
-2-9 c: ccccccccc
-`.trim()
-
-describe("day 2", () => {
-  describe("parseLine", () => {
-    it("parses a line of pasword requirements and a password into a [number, number, string, string] tuple", () => {
-      expect(parseLine("1-3 a: abcde")).toEqual([1, 3, "a", "abcde"])
-    })
-  })
-
-  describe("validateMinMax", () => {
-    it("ensures a character appears a certain number of times", () => {
-      expect(validateMinMax([1, 3, "a", "qwerty"])).toBeFalsy()
-      expect(validateMinMax([1, 3, "a", "asdf"])).toBeTruthy()
-      expect(validateMinMax([1, 3, "a", "aasdf"])).toBeTruthy()
-      expect(validateMinMax([1, 3, "a", "aaasdf"])).toBeTruthy()
-      expect(validateMinMax([1, 3, "a", "aaaasdf"])).toBeFalsy()
-    })
-  })
-
-  describe("validateCharacterLocations", () => {
-    it("ensures a character appears at one of two indices, but not both", () => {
-      expect(validateCharacterLocations([1, 3, "a", "qwerty"])).toBeFalsy()
-      expect(validateCharacterLocations([1, 3, "a", "asdf"])).toBeTruthy()
-      expect(validateCharacterLocations([1, 3, "a", "aasdf"])).toBeTruthy()
-      expect(validateCharacterLocations([1, 3, "a", "aaasdf"])).toBeFalsy()
-      expect(validateCharacterLocations([1, 3, "a", "zaasdf"])).toBeTruthy()
-      expect(validateCharacterLocations([1, 3, "a", "aaaasdf"])).toBeFalsy()
-    })
-  })
-
-  describe("part 1", () => {
-    it("finds valid sample passwords", () => {
-      expect(solve1(sample)).toEqual(2)
+describeDay(
+  2,
+  {
+    solve: solve1,
+    example: 2,
+    puzzle: 398,
+  },
+  {
+    solve: solve2,
+    example: 1,
+    puzzle: 562,
+  },
+  () => {
+    describe("parseLine", () => {
+      it("parses a line of pasword requirements and a password into a [number, number, string, string] tuple", () => {
+        expect(parseLine("1-3 a: abcde")).toEqual([1, 3, "a", "abcde"])
+      })
     })
 
-    it("finds the puzzle answer", () => {
-      expect(solve1(readInput(__dirname))).toEqual(398)
+    describe("validateMinMax", () => {
+      it("ensures a character appears a certain number of times", () => {
+        expect(validateMinMax([1, 3, "a", "qwerty"])).toBeFalsy()
+        expect(validateMinMax([1, 3, "a", "asdf"])).toBeTruthy()
+        expect(validateMinMax([1, 3, "a", "aasdf"])).toBeTruthy()
+        expect(validateMinMax([1, 3, "a", "aaasdf"])).toBeTruthy()
+        expect(validateMinMax([1, 3, "a", "aaaasdf"])).toBeFalsy()
+      })
     })
-  })
 
-  describe("part 2", () => {
-    it("finds valid sample passwords", () => {
-      expect(solve2(sample)).toEqual(1)
+    describe("validateCharacterLocations", () => {
+      it("ensures a character appears at one of two indices, but not both", () => {
+        expect(validateCharacterLocations([1, 3, "a", "qwerty"])).toBeFalsy()
+        expect(validateCharacterLocations([1, 3, "a", "asdf"])).toBeTruthy()
+        expect(validateCharacterLocations([1, 3, "a", "aasdf"])).toBeTruthy()
+        expect(validateCharacterLocations([1, 3, "a", "aaasdf"])).toBeFalsy()
+        expect(validateCharacterLocations([1, 3, "a", "zaasdf"])).toBeTruthy()
+        expect(validateCharacterLocations([1, 3, "a", "aaaasdf"])).toBeFalsy()
+      })
     })
-
-    it("finds the puzzle answer", () => {
-      expect(solve2(readInput(__dirname))).toEqual(562)
-    })
-  })
-})
+  },
+)
