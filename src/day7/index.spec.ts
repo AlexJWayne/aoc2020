ad237489c8f41a3084ac2802b99c94f7d6dd039c
Alex Wayne <alex@beautifulpixel.com>
2020-12-06 23:43:26 -0800

    Day 7 part 2

--- src/day7/index.spec.ts
+++ src/day7/index.spec.ts
@@ -1,5 +1,5 @@
 import { describeDay } from "../spec-helper"
-import { parseCountedBag, parseLines, search, solve1, solve2 } from "."
+import { count, parseCountedBag, parseLines, search, solve1, solve2 } from "."
 
 describeDay(
   7,
@@ -9,9 +9,9 @@ describeDay(
     puzzle: 229,
   },
   {
-    // solve: solve2,
-    // example: 0,
-    // puzzle: 0,
+    solve: solve2,
+    example: 126,
+    puzzle: 6683,
   },
   () => {
     describe("search", () => {
@@ -20,6 +20,7 @@ describeDay(
           search(
             {
               a: { b: 1 },
+              b: {},
             },
             "b",
           ),
@@ -32,10 +33,27 @@ describeDay(
               a: { b: 1 },
               b: { c: 2 },
               c: { d: 3 },
+              d: {},
             },
             "d",
           ),
         ).toEqual(new Set(["a", "b", "c"]))
+      })
+    })
+
+    describe("count", () => {
+      it("counts all bags in a bag including the root bag", () => {
+        expect(
+          count(
+            {
+              a: { b: 1 },
+              b: { c: 2 },
+              c: { d: 3 },
+              d: {},
+            },
+            "a",
+          ),
+        ).toEqual(10)
       })
     })
 
--- src/day7/index.ts
+++ src/day7/index.ts
@@ -6,7 +6,14 @@ interface Rules {
   }
 }
 
-// "2 muted yellow bags" => { ['muted yellow']: 2 }
+/**
+ * Parse a color and quantity of bags into an object with the color as the key,
+ * and quantity as the value.
+ *
+ *     parseCountedBag("2 muted yellow bags") //=> { ['muted yellow']: 2 }
+ *
+ * @param bagString The raw bag description in `${color}
+ */
 export function parseCountedBag(
   bagString: string,
 ): { [color: string]: number } {
@@ -15,6 +22,10 @@ export function parseCountedBag(
   return { [color]: count }
 }
 
+/**
+ * Parse the rules of the bag nesting.
+ * @param lines A string array of all nesting rules.
+ */
 export function parseLines(lines: string[]): Rules {
   const result: Rules = {}
 
@@ -35,30 +46,51 @@ export function parseLines(lines: string[]): Rules {
   return result
 }
 
+/**
+ * Find all the bags that a bag could potentially be nested inside of.
+ * @param rules The list of rules that define bag nesting.
+ * @param searchColor The bag color you are searching for.
+ */
 export function search(rules: Rules, searchColor: string): Set<string> {
   const colors: Set<string> = new Set()
 
   for (const [bagColor, contents] of Object.entries(rules)) {
-    // Check if this can contain other bags.
-    if (contents) {
-      // Check if this bag contains the requested bag.
-      if (contents[searchColor]) {
-        // Add this bag as an allowed container.
-        colors.add(bagColor)
-        // Add this bags parents as allowed containers.
-        search(rules, bagColor).forEach(parent => colors.add(parent))
-      }
+    // Check if this bag contains the requested bag.
+    if (contents[searchColor]) {
+      // Add this bag as an allowed container.
+      colors.add(bagColor)
+      // Add this bags parents as allowed containers.
+      search(rules, bagColor).forEach(parent => colors.add(parent))
     }
   }
 
   return colors
 }
 
+/**
+ * Counts the total number of bags you would have if you had a bag of a
+ * specific color.
+ * @param rules The list of rules that define bag nesting.
+ * @param parentColor The bag color to count all bags inside of.
+ */
+export function count(rules: Rules, parentColor: string): number {
+  const bag = rules[parentColor]
+
+  let total = 1
+  for (const [bagColor, qty] of Object.entries(bag)) {
+    total += qty * count(rules, bagColor)
+  }
+  return total
+}
+
+/** Solve part 1. */
 export function solve1(input: string): number {
   const lines = parseLines(input.split("\n"))
   return search(lines, "shiny gold").size
 }
 
+/** Solve part 2. */
 export function solve2(input: string): number {
-  return -1
+  const lines = parseLines(input.split("\n"))
+  return count(lines, "shiny gold") - 1
 }
