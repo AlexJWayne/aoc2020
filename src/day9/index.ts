09680c6dbd612f49f754f3f857eeedeb568dafe2
Alex Wayne <alex@beautifulpixel.com>
2020-12-08 22:50:43 -0800

    Improve comments

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,18 +1,28 @@
-/** Returns true when a number is the sum of any two items in an array of numbers */
+/**
+ * Returns true when a number is the sum of any two items in an array of numbers
+ * @param sum The sum to search for.
+ * @param numbers The array of search in.
+ */
 export function isDigitSumOfOtherDigits(
-  num: number,
+  sum: number,
   numbers: number[],
 ): boolean {
+  // For each pair of different numbers...
   for (const [i1, n1] of numbers.entries()) {
     for (const [i2, n2] of numbers.entries()) {
-      if (i1 !== i2 && n1 + n2 === num) return true
+      // Check if this pair has the desired sum.
+      if (i1 !== i2 && n1 + n2 === sum) return true
     }
   }
 
   return false
 }
 
-/** Returns the first number in `numbers` that is not a sum of of two of the previous `preambleCount` numbers. */
+/**
+ * Returns the first number in `numbers` that is not a sum of of two of the previous `preambleCount` numbers.
+ * @param preambleCount The number of digits before the current digit to search.
+ * @param numbers The array of numbers to search.
+ */
 export function findFishyNumber(
   preambleCount: number,
   numbers: number[],
@@ -28,7 +38,11 @@ export function findFishyNumber(
   throw new Error("No fishy digits!")
 }
 
-/** Finds the set of contiguous `numbers` that sum up to `sum` */
+/**
+ * Finds the set of contiguous `numbers` that sum up to `sum`
+ * @param sum The sum to search for.
+ * @param numbers The array of numbers to search.
+ */
 export function findContiguousSum(sum: number, numbers: number[]): number[] {
   let total = 0
 
